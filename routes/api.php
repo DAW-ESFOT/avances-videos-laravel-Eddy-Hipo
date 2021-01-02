afbcdee0272a25de2bf22cc64155520653e92721
Eddy Hipo <eddy.hipo@epn.edu.ec>
2021-01-01 20:01:33 -0500

    #9 Auth JWT Proteccion de rutas con middleware

--- routes/api.php
+++ routes/api.php
@@ -42,14 +42,20 @@ Route::delete('articles/{id}', function($id) {
     return 204;
 });*/
 
-
+/*
 Route::get('articles', 'App\Http\Controllers\ArticleController@index');
-
 Route::get('articles/{article}', 'App\Http\Controllers\ArticleController@show');
-
 Route::post('articles', 'App\Http\Controllers\ArticleController@store');
-
 Route::put('articles/{article}', 'App\Http\Controllers\ArticleController@update');
-
 Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete');
+*/
 
+Route::post('register', 'App\Http\Controllers\UserController@register');
+Route::post('login', 'App\Http\Controllers\UserController@authenticate');
+Route::get('articles', 'App\Http\Controllers\ArticleController@index');
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', 'App\Http\Controllers\UserController@getAuthenticatedUser');
+    Route::get('articles/{article}', 'App\Http\Controllers\ArticleController@show');Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'App\Http\Controllers\ArticleController@update');
+    Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete');
+});
