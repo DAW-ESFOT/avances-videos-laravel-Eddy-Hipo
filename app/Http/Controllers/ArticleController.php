7fd46821770a02333e4d04b4ebfacf1d16d68092
Eddy Hipo <eddy.hipo@epn.edu.ec>
2021-01-16 11:30:50 -0500

    #14 Resource (Articles)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 use App\Models\Article;
+use App\Http\Resources\Article as ArticleResource;
+use App\Http\Resources\ArticleCollection as ArticleCollection;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        return Article::all();
+        return new ArticleCollection(Article::paginate(10));
     }
     public function show(Article $article)
     {
-        return $article;
+        return response()->json(new ArticleResource($article),200);
     }
     public function store(Request $request)
     {
