b7742c32136982550ba92d74446354617749847c
Eddy Hipo <eddy.hipo@epn.edu.ec>
2021-01-16 18:06:54 -0500

    #23 Validacion de datos CommentController Validación de categoria de articulo Validación de titulo unico con excepción en actualización

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,10 +8,6 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    private static $rules = [
-        'title' => 'required|string|unique:articles|max:255',
-        'body' => 'required',
-    ];
 
     private static $messages = [
         'required' => 'El campo :attribute es obligatorio.',
@@ -33,21 +29,23 @@ class ArticleController extends Controller
             'body.required' => 'El body no es válido.',
         ];
 
-        $request->validate(self::$rules, self::$messages);
-
-        /* $validator = Validator::make($request->all(), [
+        $request->validate([
             'title' => 'required|string|unique:articles|max:255',
-            'body' => 'required|string'
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['error' => 'data_validation_failed', "error_list"=>$validator->errors()], 400);
-        } */
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id'
+        ], self::$messages);
 
         $article = Article::create($request->all());
         return response()->json($article, 201);
     }
     public function update(Request $request, Article $article)
     {
+        $request->validate([
+            'title' => 'required|string|unique:articles,title,'.$article->id.'|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id'
+        ], self::$messages);
+
         $article->update($request->all());
         return response()->json($article, 200);
     }
--- routes/api.php
+++ routes/api.php
@@ -54,9 +54,21 @@ Route::post('register', 'App\Http\Controllers\UserController@register');
 Route::post('login', 'App\Http\Controllers\UserController@authenticate');
 Route::get('articles', 'App\Http\Controllers\ArticleController@index');
 Route::group(['middleware' => ['jwt.verify']], function() {
+
     Route::get('user', 'App\Http\Controllers\UserController@getAuthenticatedUser');
+
+
+    //Articles
     Route::get('articles/{article}', 'App\Http\Controllers\ArticleController@show');
     Route::post('articles', 'App\Http\Controllers\ArticleController@store');
     Route::put('articles/{article}', 'App\Http\Controllers\ArticleController@update');
     Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete');
+
+    //Comments
+    Route::get('articles/{article}/comments', 'App\Http\Controllers\CommentController@index');
+    Route::get('articles/{article}/comments/{comment}', 'App\Http\Controllers\CommentController@show');
+    Route::post('articles/{article}/comments', 'App\Http\Controllers\CommentController@store');
+    Route::put('articles/{article}/comments/{comment}', 'App\Http\Controllers\CommentController@update');
+    Route::delete('articles/{article}/comments/{comment}', 'App\Http\Controllers\CommentController@delete');
+
 });
