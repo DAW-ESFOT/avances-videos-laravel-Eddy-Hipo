e1837e00476ca4619b7f4b55260cc76efe511669
Eddy Hipo <eddy.hipo@epn.edu.ec>
2021-01-24 15:45:09 -0500

    Envio de correos y eprsonalización de los mensajes

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\NewComment;
 use App\Models\Article;
 use App\Models\Comment;
 use App\Http\Resources\Comment as CommentResource;
-use http\Env\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
 
 class CommentController extends Controller
 {
@@ -31,7 +33,7 @@ class CommentController extends Controller
     public function show(Article $article, Comment $comment)
     {
         $comment = $article->comments()->where('id',$comment->id)->firstOrFail();
-        return response()->json($comment,200);
+        return response()->json(new CommentResource($comment),200);
     }
 
     /**
@@ -47,10 +49,9 @@ class CommentController extends Controller
             'text' => 'required|string'
         ]);
         $comment = $article->comments()->save(new Comment($request->all()));
-        return response()->json($comment, 201);
+        Mail::to($article->user)->send(new NewComment($comment));
+        return response()->json(new CommentResource($comment), 201);
     }
-
-
 
     /**
      * Update the specified resource in storage.
